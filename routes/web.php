6358c327366a91403226b08c62317bca67de7e16
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-20 23:55:12 +0100

    Parte 2 (14-11)
    Creazione rotta guest

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,8 @@ Route::middleware('auth')
     ->group(function(){
         Route::get('/', 'HomeController@index')->name('home');
     });
+
+
+Route::get("{any?}", function(){
+    return view("guest.home");
+})->where("any", ".*");
