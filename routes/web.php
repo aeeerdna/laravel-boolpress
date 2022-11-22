05c2327fb3203591e19aa23ca577ea12d3e2cc35
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-22 01:04:36 +0100

    cambio rotte

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
+
+
+Route::get('/', function () {
+    return view('guest.home');
+})->name('index');
+
 
 // admin routes
 Route::middleware('auth')
@@ -30,5 +34,6 @@ Route::middleware('auth')
 
 
 Route::get("{any?}", function(){
+    return redirect()->route('index');
     return view("guest.home");
 })->where("any", ".*");
