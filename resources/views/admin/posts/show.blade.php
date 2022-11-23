8c4331c2f9e935445b1cc787040abb563353212a
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-23 23:48:58 +0100

    delete

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -13,4 +13,12 @@
     <div class="mt-3">
         <a href="{{route('admin.posts.edit', $post->id)}}">Edit</a>
     </div>
+
+    <div class="mt-3">
+        <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <input onclick="confirm('Are you sure??')" type="submit" value="Delete">
+        </form>
+    </div>
 @endsection
