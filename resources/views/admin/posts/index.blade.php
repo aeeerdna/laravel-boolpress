882596254bd44f7656b25b00e984b9baee214fee
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-23 22:50:35 +0100

    creazione link nuovo post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.dashboard')
 
 @section('content')
+    <div class="row mt-3">
+        <div class="col-12">
+            <a href="{{route('admin.posts.create')}}">New</a>
+        </div>
+    </div>
+
     <div class="row">
         @foreach ($posts as $post)
             <div class="col-12">
