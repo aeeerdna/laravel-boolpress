f89de3b84454ed806002576d22005f0d8717b976
Andrea Miglietta <m.andrea96@hotmail.it>
2022-11-23 22:40:26 +0100

    postseeder add faker

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +11,27 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
+        for($i = 0; $i < 20; $i++){
+            $post = new Post();
+            $post->title = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $post->content = $faker->text();
+
+            $slug = Str::slug($post->title);
+            $slug_base = $slug;
+            $counter = 1;
+            $existingPost = Post::where('slug', $slug)->first();
+            while($existingPost){
+                $slug = $slug_base . '_' . $counter;
+                $existingPost = Post::where('slug', $slug)->first();
+                $counter++;
+            }
+
+            $post->slug = $slug;
+            $post->save();
+
+        }
     }
 }
